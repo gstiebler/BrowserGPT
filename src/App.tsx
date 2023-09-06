a71d7aff6b848791d2e184d2ff36d95f8a734210
Gui Stiebler <gms.public@stiebler.me>
2023-09-05 17:35:57 -0700

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import 'chrome' 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div className="App">
+     <h1>SEO Extension built with React!</h1>
+ 
+     <ul className="SEOForm">
+       <li className="SEOValidation">
+         <div className="SEOValidationField">
+           <span className="SEOValidationFieldTitle">Title</span>
+           <span className="SEOValidationFieldStatus Error">
+             90 Characters
+           </span>
+         </div>
+         <div className="SEOVAlidationFieldValue">
+           The title of the page
+         </div>
+       </li>
+ 
+       <li className="SEOValidation">
+         <div className="SEOValidationField">
+           <span className="SEOValidationFieldTitle">Main Heading</span>
+           <span className="SEOValidationFieldStatus Ok">
+             1
+           </span>
+         </div>
+         <div className="SEOVAlidationFieldValue">
+           The main headline of the page (H1)
+         </div>
+       </li>
+     </ul>
+   </div>
+ );
 }
 
+// open chrome side panel
+chrome.devtools.panels.create("SEO", null, function(panel) {
+  panel.onShown.addListener(function(window) {
+    // append react app to the panel
+    window.document.body.appendChild(App());
+  });
+});
+
+chrome.sidePanel.open({ tabId: 5});
+ 
 export default App;

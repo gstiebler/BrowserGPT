2074c24751d60a9b7a9e1796af54a78d1afb2192
Gui Stiebler <gms.public@stiebler.me>
2023-09-10 10:17:18 -0700

    Interacting with OpenAI on a chat interface, like ChatGPT

--- src/ExtTab/ExtensionTab.tsx
+++ src/ExtTab/ExtensionTab.tsx
@@ -1,36 +1,78 @@
 import React, { useState } from 'react';
-import './ExtensionTab.css';
 import { send } from '../ai/openai';
+import './ExtensionTab.css';
 
-const ExtensionTab: React.FC = () => {
-    const [apiKey, setApiKey] = useState<string>('');
+type TChatItem = {
+    type: 'user' | 'system';
+    message: string;
+};
 
-    const handleSend = async () => {
-        console.log('API Key:', apiKey);
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          const currentTab = tabs[0];
-          chrome.tabs.sendMessage(currentTab.id ?? 0, { type: 'execute' });
-        });
-        const result = await send(apiKey, "Hello!");
+function sendMessage() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentTab = tabs[0];
+        chrome.tabs.sendMessage(currentTab.id ?? 0, { type: 'execute' });
+    });
+}
+
+const ExtensionTab = () => {
+    const [apiKey, setApiKey] = useState('');
+    const [chatMessage, setChatMessage] = useState('');
+    const [chatLog, setChatLog] = useState<TChatItem[]>([]);
+
+    const handleSendMessage = async () => {
+        // sendMessage();
+
+        if (!chatMessage) {
+            return;
+        }
+
+        setChatLog([...chatLog, { type: 'user', message: chatMessage }]);
+
+        const result = await send(apiKey, chatMessage);
         console.log(JSON.stringify(result));
+        setChatLog([...chatLog, 
+            { type: 'user', message: chatMessage }, 
+            { type: 'system', message: (result.choices[0].message.content ?? "") },
+        ]);
+
+        setChatMessage('');
     };
 
     return (
-        <div className="extension-tab">
-            <p>Welcome to the Chrome Extension!</p>
-            <p>This is a simple tool to help you get started.</p>
-            <div className="input-group">
+        <div className="futuristic-container">
+            <div className="intro-text">
+                <p>Welcome to the Futuristic Interface.</p>
+                <p>Experience the next generation of UI.</p>
+            </div>
+            <div className="api-key-container">
                 <label htmlFor="api-key">API key</label>
                 <input
-                    type="text"
                     id="api-key"
+                    type="text"
                     value={apiKey}
-                    onChange={(e) => setApiKey(e.target.value)}
+                    onChange={e => setApiKey(e.target.value)}
                 />
             </div>
-            <button onClick={handleSend}>Send</button>
+            <div className="chat-interface">
+                <div className="chat-log">
+                    {chatLog.map((entry, index) => (
+                        <p key={index} className={entry.type}>
+                            {entry.message}
+                        </p>
+                    ))}
+                </div>
+                <div className="chat-input">
+                    <input
+                        type="text"
+                        value={chatMessage}
+                        onChange={e => setChatMessage(e.target.value)}
+                        placeholder="Type a message..."
+                    />
+                    <button onClick={handleSendMessage}>Send</button>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default ExtensionTab;

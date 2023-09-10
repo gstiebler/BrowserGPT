c03cd05a9e18bbe607984836135ac872fac2040f
Gui Stiebler <gms.public@stiebler.me>
2023-09-10 09:20:00 -0700

    moves

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './ExtensionTab';
+import App from './ExtTab/ExtensionTab';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(

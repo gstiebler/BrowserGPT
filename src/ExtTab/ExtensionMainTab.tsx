e3d39202850c1862ab49bf51bc5ac3459dfef02b
Gui Stiebler <gms.public@stiebler.me>
2023-09-23 17:46:05 -0700

    fix spacing

--- src/ExtTab/ExtensionMainTab.tsx
+++ src/ExtTab/ExtensionMainTab.tsx
@@ -18,49 +18,49 @@ const ExtensionMainTab: React.FC<ExtensionMainTabProps> = ({
     chatMessage,
     setChatMessage,
     handleSendMessage,
-  }) => {
+}) => {
     return (
-      <Container maxWidth="md">
-        <Paper elevation={3} style={{ padding: '20px' }}>
-          <Typography variant="h5">Welcome to the Futuristic Interface.</Typography>
-          <Typography variant="body1">Experience the next generation of UI.</Typography>
-          
-          <div style={{ margin: '20px 0' }}>
-            <TextField
-              label="API Key"
-              variant="outlined"
-              fullWidth
-              value={apiKey}
-              onChange={e => setApiKey(e.target.value)}
-            />
-          </div>
-          
-          <div style={{ margin: '20px 0' }}>
-            <Typography variant="body1">Chat Log</Typography>
-            <Paper elevation={1} style={{ padding: '10px', height: '200px', overflow: 'auto' }}>
-              {chatLog.map((entry, index) => (
-                <Typography key={index} variant="body2" style={{ textAlign: entry.role === 'user' ? 'right' : 'left' }}>
-                  {entry.message}
-                </Typography>
-              ))}
+        <Container maxWidth="md">
+            <Paper elevation={3} style={{ padding: '20px' }}>
+                <Typography variant="h5">Welcome to the Futuristic Interface.</Typography>
+                <Typography variant="body1">Experience the next generation of UI.</Typography>
+
+                <div style={{ margin: '20px 0' }}>
+                    <TextField
+                        label="API Key"
+                        variant="outlined"
+                        fullWidth
+                        value={apiKey}
+                        onChange={e => setApiKey(e.target.value)}
+                    />
+                </div>
+
+                <div style={{ margin: '20px 0' }}>
+                    <Typography variant="body1">Chat Log</Typography>
+                    <Paper elevation={1} style={{ padding: '10px', height: '200px', overflow: 'auto' }}>
+                        {chatLog.map((entry, index) => (
+                            <Typography key={index} variant="body2" style={{ textAlign: entry.role === 'user' ? 'right' : 'left' }}>
+                                {entry.message}
+                            </Typography>
+                        ))}
+                    </Paper>
+                </div>
+
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '10px' }}>
+                    <TextField
+                        label="Type a message..."
+                        variant="outlined"
+                        style={{ flex: 1 }} // Takes up the available space
+                        value={chatMessage}
+                        onChange={e => setChatMessage(e.target.value)}
+                    />
+                    <Button variant="contained" color="primary" onClick={handleSendMessage}>
+                        Send
+                    </Button>
+                </div>
             </Paper>
-          </div>
-          
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <TextField
-              label="Type a message..."
-              variant="outlined"
-              fullWidth
-              value={chatMessage}
-              onChange={e => setChatMessage(e.target.value)}
-            />
-            <Button variant="contained" color="primary" onClick={handleSendMessage}>
-              Send
-            </Button>
-          </div>
-        </Paper>
-      </Container>
+        </Container>
     );
-  };
+};
 
 export default ExtensionMainTab;

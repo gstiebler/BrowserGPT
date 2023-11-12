715bf014235cea9c19b27856a7bc8309f6182cd5
Gui Stiebler <gms.public@stiebler.me>
2023-11-11 18:54:33 -0800

    added logs

--- src/html/browserDriver.ts
+++ src/html/browserDriver.ts
@@ -14,15 +14,18 @@ export function getSummarizedHtmlFromDocument(): TSummarizedHtml {
 export function setInputValue(id: string, value: string, extractor: HtmlExtraction) {
     const realId = extractor.getRealId(id);
     const input = document.getElementById(realId) as HTMLInputElement;
+    console.log(`Setting the value ${value} to the input ${realId}`);
     input.value = value;
 }
 
 export function openLink(url: string) {
+    console.log(`Opening the link ${url}`);
     window.location.href = url;
 }
 
 export function clickButton(id: string, extractor: HtmlExtraction) {
     const realId = extractor.getRealId(id);
     const button = document.getElementById(realId) as HTMLButtonElement;
+    console.log(`Clicking the button ${realId}`);
     button.click();
 }

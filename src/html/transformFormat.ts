b68bfeea0bda931d70948b795973d94cb43b1821
Gui Stiebler <gms.public@stiebler.me>
2024-10-08 11:58:24 -0700

    improved last transform step

--- src/html/transformFormat.ts
+++ src/html/transformFormat.ts
@@ -1,25 +1,44 @@
 import _ from "lodash";
 import { HtmlJsonNode } from "./DomToJson";
 
+function countNonEmptyProperties(obj: any) {
+    return _.reduce(obj, (acc, value) => acc + (_.isEmpty(value) ? 0 : 1), 0);
+}
+
+const textProperties = new Set(["text", "content", "aria-label", "alt", "title"]);
+
 function transformFormatRecursive(node: any): any {
     if (_.isString(node)) {
         return node;
     }
-    const processedChildren = (node.children ?? []).map(transformFormatRecursive);
-    
+
+    const processedChildren = (node.children ?? [])
+        .map(transformFormatRecursive)
+        .filter((child: any) => !_.isEmpty(child));
+
     const { text, children, attributes, ...propsNode } = node;
-    if (_.isEmpty(propsNode) && _.isEmpty(processedChildren) && !_.isEmpty(text)) {
+    if (_.isEmpty(propsNode) && _.isEmpty(processedChildren) && _.isEmpty(attributes) && !_.isEmpty(text)) {
         return text;
     }
 
+    const mainOutput = {
+        ...propsNode,
+        ...attributes,
+    };
+
     const result = [
-        {
-            ...propsNode,
-            ...attributes,
-        },
         ...(_.isEmpty(text) ? [] : [text]),
         ...processedChildren
     ];
+    const nonEmptyPropertyCount = countNonEmptyProperties(mainOutput);
+    if (nonEmptyPropertyCount === 0) {
+
+    } else if (nonEmptyPropertyCount === 1 && textProperties.has(_.keys(mainOutput)[0])) {
+        result.push(mainOutput[_.keys(mainOutput)[0]]);
+    } else {
+        result.push(mainOutput);
+    }
+
     return result;
 }
 

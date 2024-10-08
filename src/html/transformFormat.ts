be98a25e872c5a47602026c37ecbcc31dcfaead9
Gui Stiebler <gms.public@stiebler.me>
2024-10-08 16:02:11 -0700

    improved transform formatter

--- src/html/transformFormat.ts
+++ src/html/transformFormat.ts
@@ -1,8 +1,13 @@
 import _ from "lodash";
 import { HtmlJsonNode } from "./DomToJson";
 
-function countNonEmptyProperties(obj: any) {
-    return _.reduce(obj, (acc, value) => acc + (_.isEmpty(value) ? 0 : 1), 0);
+function getNonEmptyProperties(obj: any) {
+    return Object.entries(obj)
+        .filter(([key, value]) => !_.isEmpty(value))
+        .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+        }, {} as { [key: string]: any });
 }
 
 const textProperties = new Set(["text", "content", "aria-label", "alt", "title"]);
@@ -26,19 +31,23 @@ function transformFormatRecursive(node: any): any {
     const result = [
         ...processedChildren
     ];
-    const nonEmptyPropertyCount = countNonEmptyProperties(mainOutput);
-    if (nonEmptyPropertyCount === 0) {
+    const nonEmptyProperties = getNonEmptyProperties(mainOutput);
+    const mainOutputKeys = _.keys(nonEmptyProperties);
+    if (mainOutputKeys.length === 0) {
 
-    } else if (nonEmptyPropertyCount === 1 && textProperties.has(_.keys(mainOutput)[0])) {
-        result.push(mainOutput[_.keys(mainOutput)[0]]);
+    } else if (mainOutputKeys.length === 1) {
+        const firstKey = mainOutputKeys[0];
+        if (textProperties.has(firstKey)) {
+            result.push(mainOutput[firstKey]);
+        } else {
+            result.push(mainOutput);
+        }
     } else {
         result.push(mainOutput);
     }
 
     if (result.length === 1) {
-        if (_.isArray(result[0])) {
-            return result[0];
-        }
+        return result[0];
     }
     return result;
 }
